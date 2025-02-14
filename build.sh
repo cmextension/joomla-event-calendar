d65890757952a25f13711be9adaa43ffa0cd2248
Hung Tran <nguyentieuhau@gmail.com>
2025-02-14 15:37:54 +0700

    Updated Calendar, verion and build script

--- build.sh
+++ build.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 
 buildDir="build"
-version="0.3.0"
+version="0.4.0"
 
-if [ ! -d $buildDir ]; then
-    echo "$buildDir doesn't exist"
-
-    if mkdir -p $buildDir; then
-        echo "Created $buildDir successfully"
-    else
-        echo "Creating $buildDir failed"
-        echo "Exit..."
-        exit 1
+if [ -d $buildDir ]; then
+    if rm -rf $buildDir; then
+        echo "Deleted $buildDir successfully"
     fi
 fi
+
+if mkdir -p $buildDir; then
+    echo "Created $buildDir successfully"
+else
+    echo "Creating $buildDir failed"
+    echo "Exit..."
+    exit 1
+fi
+
 
 sh ./build-scripts/com_eventcalendar.sh -v $version -d $buildDir \
     && sh ./build-scripts/mod_eventcalendar.sh -v $version -d $buildDir \
